2300b92ee053c114ee87f20a3bda9cd4e150332d
TSUNEKO <nadinelecpa87@gmail.com>
2020-10-05 05:08:28 +0200

    navbar modified

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-//var offsets = document.getElementsById("header").getBoundingClientRect();
-//var top = offsets.top;
-//var left = offsets.left;
-//console.log(offsets)
-
-
-
-
-//function getPos(header) {
-    // yay readability
-    //for (var lx=0, ly=0;
-         //header != null;
-         //lx += header.offsetLeft, ly += header.offsetTop, header = header.offsetParent);
-    //return {x: lx,y: ly};
-//}
-
 
 // 1 - Recupérer la position du bottom du header
-const header = document.getElementById("header")
-let bottomHeaderPosition = header.offsetTop
-console.log(bottomHeaderPosition)
+//const header = document.getElementById("header")
+//let bottomHeaderPosition = header.offsetTop
+//console.log(bottomHeaderPosition)
+
 
 // 2 - Get scroll event on page
 // --> console.log(" i am scrolling "") add listener on body or window ?
 
 
-
-window.addEventListener('scroll', (event) => {
-    const header  = window.scrollY
-    console.log(header);
-});
-
-
+//window.addEventListener('scroll', (event) => {
+    //console.log("iam scrolling")
+//});
 
 
 // 3 - Get window position while scrolling
+
+window.addEventListener('scroll', (event) => {
+    let main = window.scrollY;
+    console.log(main)
+});
+
+
+window.addEventListener('scroll', (event) => {
+
+    const main = document.getElementById("main")
+    const navbar = document.getElementById("navbar")
+     if(window.scrollY >= main.offsetTop) {
+        navbar.style.position = "fixed"
+        }
+    else{
+        navbar.style.position = "relative";
+    }
+});
+
 
