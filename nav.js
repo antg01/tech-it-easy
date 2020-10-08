20934cedd19949ad23f13b99fba01659473b4e1a
KillianFrappartDev <KillianFrappartDev@gmail.com>
2020-10-08 12:01:08 +0200

    nav animation

--- nav.js
+++ nav.js
@@ -3,20 +3,20 @@
 window.addEventListener('scroll', (event) => {
   let main = window.scrollY;
   let header = window.scrollY;
-  console.log(header)
+  console.log(header);
 });
 
 window.addEventListener('scroll', (event) => {
   const main = document.getElementById('main');
   const navbar = document.getElementById('navbar');
   if (window.scrollY >= main.offsetTop) {
-   navbar.style.position = 'fixed';
-   navbar.querySelector(".image-logo").style.display = 'block';
-
+    navbar.style.position = 'fixed';
+    navbar.classList.add('navbar-enter');
+    navbar.querySelector('.image-logo').style.display = 'block';
   } else {
     navbar.style.position = 'relative';
-    navbar.querySelector(".image-logo").style.display = 'none';
-    
+    navbar.querySelector('.image-logo').style.display = 'none';
+    navbar.classList.remove('navbar-enter');
   }
 });
 
